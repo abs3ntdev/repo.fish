3564f1c2758677575710bd218ffb1591ee70be92
abs3nt <abs3nt@asdf.cafe>
2025-03-05 10:48:32 -0800

    maybe better

--- functions/_repo_list.fish
+++ functions/_repo_list.fish
@@ -1,6 +1,3 @@
 function _repo_list
-    for git_dir in (find "$REPO_BASE_DIR" -type d -name ".git")
-        set parent_dir (path dirname "$git_dir")
-        echo (path basename "$parent_dir" | string replace -a "." "_")
-    end
+    find "$REPO_BASE_DIR" -type d -name ".git" -printf "%h\n" | sed 's|.*/||; s/\./_/g'
 end
