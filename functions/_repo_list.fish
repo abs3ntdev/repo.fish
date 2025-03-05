5705a8034de6029b6b313d82d04498e6f3552ef4
abs3nt <abs3nt@asdf.cafe>
2025-03-05 11:10:25 -0800

    fish

--- functions/_repo_list.fish
+++ functions/_repo_list.fish
@@ -1,3 +1,5 @@
 function _repo_list
-    find "$REPO_BASE_DIR" -type d -name ".git" -printf "%h\n" 2>/dev/null | sed 's|.*/||; s/\./_/g'
+    find "$REPO_BASE_DIR" -type d -name ".git" -printf "%h\n" 2>/dev/null | while read -l dir
+        echo (string replace -a '.' '_' (basename "$dir"))
+    end
 end
