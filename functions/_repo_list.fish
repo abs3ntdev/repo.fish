4db3329d686ac7b1dec219149ef7b7d19c572c48
abs3nt <abs3nt@asdf.cafe>
2025-03-05 10:50:25 -0800

    no errors

--- functions/_repo_list.fish
+++ functions/_repo_list.fish
@@ -1,3 +1,3 @@
 function _repo_list
-    find "$REPO_BASE_DIR" -type d -name ".git" -printf "%h\n" | sed 's|.*/||; s/\./_/g'
+    find "$REPO_BASE_DIR" -type d -name ".git" -printf "%h\n" 2>/dev/null | sed 's|.*/||; s/\./_/g'
 end
